c06a9055b1d7ac3be06b849fc0e938f11889a5d7
Vikram Singh Rana <116163333+vikramintech@users.noreply.github.com>
2023-11-14 17:04:00 +0000

    index.js updated

--- index.js
+++ index.js
@@ -49,6 +49,52 @@ function skippingLastTable(num) {
   }
 }
 
-console.log("skipping multiples of 9 and 10 from Table")
+console.log("skipping multiples of 9 and 10 from Table");
 // invoking skippingLastTable()
 skippingLastTable(4);
+
+// logging only 5 fields  in a multiple using condition
+
+function fiveTable(num) {
+  for (let i = 1; i <= 10; i++) {
+    if (i > 5) {
+      return;
+    }
+    console.log(`${num} X ${i} = ${num * i}`);
+  }
+}
+
+console.log("multiples of a number given upto 5");
+// invoking fiveTable()
+fiveTable(4);
+
+// using or condition in loop
+
+function getEven(num) {
+  for (i = 2; i <= 40; i += 2) {
+    if (i === 20 || i === 28 || i === 34) {
+      continue;
+    }
+    console.log(i);
+  }
+}
+
+console.log("skipping 20,28 and 34 using or in condition");
+// ivoking getEven()
+getEven(4);
+
+// using and condition in loop
+
+function paintNumbers(num) {
+  for (let i = 1; i <= 40; i++) {
+    if (i >= 23 && i <= 30) {
+      continue;
+    }
+    console.log(i);
+  }
+}
+
+console.log("skipping 23 to 30 usiong and in condition")
+// invoking paintNumbers()
+
+paintNumbers(4);
